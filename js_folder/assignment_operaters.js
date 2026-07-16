de57c1fb021bdd0185a63840e7b965f9c3ce83a5
ankityaduvanshi46 <ankityadavlko46@gmail.com>
2026-07-16 13:50:00 +0530

    git update

---
+++ js_folder/assignment_operaters.js
@@ -0,0 +1,12 @@
+let num =12;
+console.log(num);
+num += 5;
+console.log(num);
+num -= 4;
+console.log(num);
+num *= 2;
+console.log(num);
+num /= 5;
+num %= 2;
+console.log(num);
+
---
+++ js_folder/sasta_calc.js
@@ -0,0 +1,5 @@
+let English = 80;
+let Maths = 90;
+let Science = 70;
+let total_marks = (((English + Maths + Science) * 100)/300);
+console.log(total_marks)
