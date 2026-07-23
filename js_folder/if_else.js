a582c683e8c809387e6b914b1606b275fc2f7c39
ankityaduvanshi46 <ankityadavlko46@gmail.com>
2026-07-23 10:36:44 +0530

    merge both branch

---
+++ js_folder/if_else.js
@@ -0,0 +1,74 @@
+// // // // // let a = 18
+// // // // // if(a>18 && a<150){
+// // // // //     console.log("You are eligible for blast")
+// // // // // }
+// // // // // else{
+// // // // //     console.log("You are eligible for blast")
+// // // // // }
+
+
+
+// // // // let age =prompt ( "Enter your age")
+
+// // // //         if(age>18 && age<150){
+// // // //         alert("woow ab tum uda do desh ko")
+// // // // }
+// // // //  else{
+// // // //     alert("Tum budha hai bhai")
+// // // // }
+
+// // // let a =Number(prompt("enter your first number"))
+// // // let b =Number(prompt("emter your second number"))
+
+// // // const c=a+b
+
+// // // alert(c)
+
+
+
+// // let marks=600
+// // if(marks>90){
+// //     alert("A+");
+// // }
+// // else if(marks>80){
+// //     alert("A");
+// // }
+// // else if(marks>70){
+// //     alert("B");
+// // }
+// // else{
+// //     alert("Fail")
+// // }
+
+
+
+// let marks=prompt("Enter Your Marks")
+
+// // let marks=100
+// if(marks>90 && marks<100){
+//     alert("A+");
+// }
+// else if(marks>=80 && marks<90){
+//      alert("A");
+// }
+// else if(marks>70 && marks<80){
+//      alert("B");
+// }
+// else{
+//      alert("Fail")
+// }
+
+
+let age = 80
+if(age<=0 || age>100){
+    console.log("Enter valid age")
+}
+else if(age>=1 && age<=18){
+    console.log("You are under age")
+}
+else if(age>=19 && age<=60){
+    console.log("You are elagible")
+}
+else{
+    console.log("You are over-age")
+}
---
+++ js_folder/logic_operater.js
@@ -0,0 +1,10 @@
+let num1 = 34
+let num2 = 45
+let num3 = 23
+let num4 = 56
+
+console.log(num1>18 && num2>10 && num3>1) //true
+
+console.log(num1>18 || num2>80 || num3>1) //true
+
+console.log((num1>18 && num2>100 && num3>1) || (num1>18 || num2>80 || num3>2)) //true
